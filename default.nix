85094202ab66d4e8deb8f7514d0ba51e807b9fe8
lucas8 <luc.linux@mailoo.org>
2017-06-06 14:25:53 +0200

    Add building system

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, alex, algebraic-graphs, array, base
+      , containers, happy, mtl, stdenv, transformers
+      }:
+      mkDerivation {
+        pname = "absint";
+        version = "0.0.1.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          algebraic-graphs array base containers mtl transformers
+        ];
+        executableToolDepends = [ alex happy ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
