f10aeb070b288b6c4927336ea9fa332d3e2fcf32
lucas8 <luc.linux@mailoo.org>
2017-06-13 23:23:48 +0200

    Fix Iterator handling of functions

--- src/Iterator.hs
+++ src/Iterator.hs
@@ -24,9 +24,18 @@ class Abstract a where
     backport :: [EVarID] -> a -> a -> a
 
 iterate :: (Show a, Abstract a) => Program -> Map NodeID a
-iterate program = M.fromList $ runST $ worklist program >>= tolist
+iterate program = M.fromList $ runST $ iterator >>= tolist
  where tolist :: HT.HashTable s NodeID a -> ST s [(NodeID, a)]
        tolist = HT.foldM (\l e -> return $ e : l) []
+       iterator :: (Show a, Abstract a) => ST s (HT.HashTable s NodeID a)
+       iterator = do
+           abstract <- HT.new
+           worklist program (program_init_entry program) abstract
+           let main_entry = function_entry $ getFunByName program "main"
+           abst <- queryOrInit bottom (program_init_exit program) abstract
+           HT.insert abstract main_entry abst
+           worklist program main_entry abstract
+           return abstract
 
 queryOrInit :: (Eq k, HSH.Hashable k) => v -> k -> HT.HashTable s k v -> ST s v
 queryOrInit def key hash = do
@@ -41,28 +50,22 @@ while condition action = condition >>= \b -> if b then action >> while condition
 type Queue s      = R.STRef s (S.Seq NodeID)
 type Mark s       = HT.HashTable s NodeID Bool
 type Count s      = HT.HashTable s NodeID Integer
-type Return s     = HT.HashTable s NodeID (Maybe (NodeID, Maybe EdgeID))
-type AtReturn s a = HT.HashTable s EdgeID a
 type AbsTable s a = HT.HashTable s NodeID a
-worklist :: (Show a, Abstract a) => Program -> ST s (HT.HashTable s NodeID a)
-worklist program = do
+worklist :: (Show a, Abstract a) => Program -> NodeID
+         -> HT.HashTable s NodeID a -> ST s () -- Hashtable is changed by side-effect
+worklist program firstNode abstract = do
     -- Global variables
     queue    <- R.newSTRef S.empty -- :: Queue s
     asserts  <- R.newSTRef []
     inQueue  <- HT.new             -- :: Mark s
     nbSeens  <- HT.new             -- :: Count s
-    abstract <- HT.new             -- :: AbsTable s a
-    returns  <- HT.new             -- :: Return s
-    atReturn <- HT.new             -- :: AtReturn s a
     first    <- HT.new             -- :: Mark s
 
     -- Initialisation
-    enqueue queue inQueue $ program_init_entry program
-    let main_entry = function_entry $ getFunByName program "main"
-    setReturn returns (program_init_exit program) main_entry Nothing
+    enqueue queue inQueue firstNode
 
     let update = updateNode program queue inQueue first abstract
-    let absEdge = abstractEdge program queue inQueue returns abstract asserts atReturn
+    let absEdge = abstractEdge program queue inQueue abstract asserts
     while (emptyNQueue queue) $ do
         Just dst <- dequeue queue inQueue
         traceM $ "Handling " ++ show dst
@@ -76,22 +79,7 @@ worklist program = do
         -- Abstract instruction
         update dst final $ absEdge dst
 
-        -- Handle returns
-        mret <- popReturn returns dst
-        case mret of
-         Nothing   -> return ()
-         -- Act like a virtual EInop
-         Just (node,Nothing) -> do a <- queryOrInit bottom dst abstract
-                                   HT.insert abstract node a
-                                   enqueue queue inQueue node
-         Just (node,Just edge) -> do
-             dst_abs <- queryOrInit bottom dst  abstract
-             old_abs <- queryOrInit bottom edge atReturn
-             if isEq dst_abs old_abs then return ()
-                                     else do HT.insert atReturn edge dst_abs
-                                             enqueue queue inQueue node
-
-    return abstract
+    return ()
  where dequeue :: Queue s -> Mark s -> ST s (Maybe EdgeID)
        dequeue queue inQueue = do
            q <- R.readSTRef queue
@@ -116,12 +104,6 @@ worklist program = do
            v <- queryOrInit 0 node nbSeens
            HT.insert nbSeens node $ v + 1
 
-       setReturn :: Return s -> NodeID -> NodeID -> Maybe EdgeID -> ST s ()
-       setReturn hash key v1 v2 = HT.insert hash key $ Just (v1, v2)
-
-       popReturn :: Return s -> NodeID -> ST s (Maybe (NodeID, Maybe EdgeID))
-       popReturn hash key = queryOrInit Nothing key hash
-
        isEq :: Abstract a => a -> a -> Bool
        isEq a1 a2 = subset a1 a2 && subset a2 a1
 
@@ -133,25 +115,31 @@ worklist program = do
            abst <- queryOrInit bottom src abstract
            return (abst, edge_inst edge)
 
-       abstractEdge :: Abstract a => Program -> Queue s -> Mark s -> Return s
-                    -> AbsTable s a -> R.STRef s [EdgeExpr] -> AtReturn s a
+       abstractEdge :: (Show a, Abstract a) => Program -> Queue s -> Mark s
+                    -> AbsTable s a -> R.STRef s [EdgeExpr]
                     -> NodeID -> EdgeID -> (a, EdgeInst) -> ST s a
-       abstractEdge program queue inQueue returns abstract asserts retabs dst eid (abst, inst) =
+       abstractEdge program queue inQueue abstract asserts dst eid (abst, inst) =
            case inst of
             EIassign vid expr -> return $ assign vid expr abst
             EInop             -> return $ abst
             EIguard expr      -> return $ guard expr abst
 
+            -- This method could handle recursive function, but won't terminate.
+            -- Thus, to handle recursive functions, all that is needed is to ensure
+            -- terminaison of analysis of those (for example by not making the recursive
+            -- call in bottom environmnent, more another check).
             EIcall fun        -> do
                 let function = getFunByID program fun
                 let beg      = function_entry function
                 let end      = function_exit  function
                 let ret      = function_ret   function
-                HT.insert abstract beg abst
-                enqueue queue inQueue beg
-                setReturn returns end dst $ Just eid
-                reta <- queryOrInit bottom eid retabs
-                return $ backport (ret : program_top_vars program) reta abst
+                -- Copy the abstract
+                abstract' <- HT.new
+                HT.mapM_ (\(k,v) -> HT.insert abstract' k v) abstract
+                HT.insert abstract' beg abst
+                worklist program beg abstract'
+                abst' <- queryOrInit bottom end abstract'
+                return $ backport (ret : program_top_vars program) abst' abst
 
             EIassert expr     -> do
                 let a' = guard expr abst
